427ed84f56c1fcc1a3466981eb0ad2670d8b1c2b
Juan Mitriatti <juan.mitriatti@gmail.com>
2019-12-07 15:21:06 -0300

    Module Hero : Hooks and events. Event subscriber

---
+++ modules/custom/module_hero/src/EventSubscriber/HeroInitSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\module_hero\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+/**
+ * Our events suscriber;
+ */
+class HeroInitSubscriber implements EventSubscriberInterface {
+
+
+  public function __construct( ) {
+
+  }
+
+  public function onRequest($event) {
+    var_dump("Hello from our event");
+  }
+
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['onRequest'];
+    return $events;
+  }
+}
