d5db0ab32a182dec6f32ebdb2f6c5019df413150
Juan Mitriatti <juan.mitriatti@gmail.com>
2019-12-07 17:39:45 -0300

    Module hero : Declare and implement custom permission

--- modules/custom/module_hero/src/Controller/HeroController.php
+++ modules/custom/module_hero/src/Controller/HeroController.php
@@ -13,21 +13,27 @@ use Drupal\Core\Config\ConfigFactory;
 class HeroController extends ControllerBase {
 
   private $articleHeroService;
+
+  protected $currentUser;
+
   protected $configFactory;
 
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('module_hero.hero_articles'),
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('current_user')
     );
   }
 
-  public function __construct(HeroArticleService $articleHeroService, ConfigFactory $configFactory) {
+  public function __construct(HeroArticleService $articleHeroService, ConfigFactory $configFactory, $currentUser) {
     $this->articleHeroService = $articleHeroService;
     $this->configFactory = $configFactory;
+    $this->currentUser = $currentUser;
   }
 
   public function heroList() {
+   // kint($this->currentUser);
 
     $heroes = [
       ['name' => 'HULK'],
@@ -38,10 +44,22 @@ class HeroController extends ControllerBase {
       ['name' => 'Wolverine'],
     ];
 
-    return [
-      '#theme' => 'hero_list',
-      '#items' => $heroes,
-      '#title' => $this->configFactory->get('module_hero.settings')->get('hero_list_title')
-    ];
+    if ($this->currentUser->hasPermission('can see hero list')) {
+      return [
+        '#theme' => 'hero_list',
+        '#items' => $heroes,
+        '#title' => $this->configFactory->get('module_hero.settings')
+          ->get('hero_list_title'),
+      ];
+    }
+    else {
+      return [
+        '#theme' => 'hero_list',
+        '#items' => [],
+        '#title' => $this->configFactory->get('module_hero.settings')
+          ->get('hero_list_title'),
+      ];
+    }
+
   }
 }
